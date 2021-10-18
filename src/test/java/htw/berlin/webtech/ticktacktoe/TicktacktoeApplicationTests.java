de89e829878be432cd9a83e4c54a08f26c2b0141
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-10-18 16:22:59 +0200

    Adds Spring Boot.

---
+++ src/test/java/htw/berlin/webtech/ticktacktoe/TicktacktoeApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.ticktacktoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktacktoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
