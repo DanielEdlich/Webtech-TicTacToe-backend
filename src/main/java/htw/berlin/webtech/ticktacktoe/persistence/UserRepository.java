ccd7b68c028e5400d151422fef10439c4474238f
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-11-06 14:53:41 +0100

    Add annotation.

--- src/main/java/htw/berlin/webtech/ticktacktoe/persistence/UserRepository.java
+++ src/main/java/htw/berlin/webtech/ticktacktoe/persistence/UserRepository.java
@@ -2,9 +2,11 @@ package htw.berlin.webtech.ticktacktoe.persistence;
 
 import htw.berlin.webtech.ticktacktoe.api.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     List<User> findAllByName(String Name);
